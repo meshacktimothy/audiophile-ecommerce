2f9a39f4ea7455c937faa4e9fb8d96b586de6533
christianahade <olasubomistephen8@gmail.com>
2025-10-02 13:16:41 +0100

    index.tsx styles.ts

--- src/pages/home/FirstSpeaker/index.tsx
+++ src/pages/home/FirstSpeaker/index.tsx
@@ -2,6 +2,8 @@ import {
   SpeakerDiv,
   SpeakerText,
   FirstSpeakerImg,
+  SpeakerIconDiv,
+  FirstSpeakerContent,
   // SpeakerIconDiv,
 } from "./styles";
 import HandponeButton from "../../../components/Botton";
@@ -12,19 +14,22 @@ export default function Index() {
   return (
     <>
       <SpeakerDiv>
-        {/* <SpeakerIconDiv>  */}
-        <FirstSpeakerImg src={bigSpeaker}></FirstSpeakerImg>
+        <SpeakerIconDiv>
           <SpeakerIcon />
-        <SpeakerText>
-          <h1>
-            ZX9 <br /> SPEAKER
-          </h1>
-          <p>
-            Upgrade to premium speakers that are <br /> phenomenally built to
-            deliver truly remarkable <br /> sound.
-          </p> 
-          <HandponeButton />
-        </SpeakerText>
+        </SpeakerIconDiv>
+        <FirstSpeakerContent>
+          <FirstSpeakerImg src={bigSpeaker}></FirstSpeakerImg>
+          <SpeakerText>
+            <h1>
+              ZX9 <br /> SPEAKER
+            </h1>
+            <p>
+              Upgrade to premium speakers that are <br /> phenomenally built to
+              deliver truly remarkable <br /> sound.
+            </p>
+            <HandponeButton />
+          </SpeakerText>
+        </FirstSpeakerContent>
       </SpeakerDiv>
     </>
   );
--- src/pages/home/FirstSpeaker/styles.ts
+++ src/pages/home/FirstSpeaker/styles.ts
@@ -1,57 +1,61 @@
 import styled from "@emotion/styled";
-import colors from "../../../styles/color"
-
-
+import colors from "../../../styles/color";
 
 export const SpeakerDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background-color: ${colors.primary01};
-    border-radius: 8px;
-    height: 560px;
-    margin: 0 24px;
-    padding: 0 24px;
-    // gap: 125px;
-    margin-top: 80px;
-    margin-bottom: 80px;
-    // position: relative;
-    overflow: hidden; 
-`
+//   display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.primary01};
+  border-radius: 8px;
+  height: 560px;
+  margin: 0 24px;
+  padding: 0 24px;
+  // gap: 125px;
+  margin-top: 80px;
+  margin-bottom: 80px;
+  position: relative;
+  overflow: hidden;
+`;
 
 export const SpeakerText = styled.div`
-    color: white;
-    position:absolute;
-    text-align: left;
-    margin-left:40%;
-    h1  
-    {
-        font-size: 56px;
-        line-height: 58px;
-        font-weight: 700;
-        letter-spacing: 2px;
-        margin-bottom: 32px;
-    }       
-    p  {
-        font-size: 15px;
-        line-height: 25px;
-        opacity: 0.75;
-        margin-bottom: 40px;
-    }
-`
+  color: white;
+  text-align: left;
+  margin-left: 15%;
+margin-top:80px;
+  width:100%;
+  h1 {
+    font-size: 56px;
+    line-height: 58px;
+    font-weight: 700;
+    letter-spacing: 2px;
+    margin-bottom: 32px;
+  }
+  p {
+    font-size: 15px;
+    line-height: 25px;
+    opacity: 0.75;
+    margin-bottom: 40px;
+  }
+`;
 
 export const FirstSpeakerImg = styled.img`
-    width: 410px;
-    height: 488px;
-    margin-top: 100px;
-    margin-left:130px;
-    zindex: -1;
-    `
+  width: 410px;
+  height: 488px;
+  margin-top: 100px;
+  margin-left:100px;
+  
+`;
 
 export const SpeakerIconDiv = styled.div`
-position: absolute;
-overflow: hidden;
-   left:-100px
-   background-color:red;
-    `
+  position: absolute;
+margin:-15% 20px;
+  left: -50px;
+`;
+
+export const FirstSpeakerContent = styled.div`
+  position: absolute;
+display:flex;
+gap:30px;
+width:100%;     
+`
